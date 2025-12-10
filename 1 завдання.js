5e97f01db82ed291cf38479c30ebd7a7a43a0e9c
nataliianadychpp2024-hash <nataliia.nadych.pp.2024@lpnu.ua>
2025-12-10 14:09:19 +0200

    Update and rename 11 завдання.js to 1 завдання.js

---
+++ 1 завдання.js
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="uk">
+<head>
+  <meta charset="utf-8" />
+  <title>Найдовша назва виду спорту</title>
+</head>
+<body>
+  <h1>Найдовша назва</h1>
+
+  <script>
+    
+  </script>
+</body>
+</html>
