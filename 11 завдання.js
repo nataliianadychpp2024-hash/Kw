eaf5d0e6081a349993681af3b77082ebcbb95ee5
nataliianadychpp2024-hash <nataliia.nadych.pp.2024@lpnu.ua>
2025-12-10 14:07:31 +0200

    Create 11 завдання.js

---
+++ 11 завдання.js
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="uk">
+<head>
+  <meta charset="utf-8" />
+  <title>Гра: вгадай спорт</title>
+</head>
+<body>
+  <h1>Гра: вгадай вид спорту</h1>
+  <button id="startBtn">Почати гру</button>
+
+  <script>
+    
+  </script>
+</body>
+</html>
