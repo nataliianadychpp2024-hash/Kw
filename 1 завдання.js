b10a9c8d65ad3eaf70d7ccf8de334f309f9c06ae
nataliianadychpp2024-hash <nataliia.nadych.pp.2024@lpnu.ua>
2025-12-10 14:14:00 +0200

    Add script to sort and display sports names

--- 1 завдання.js
+++ 1 завдання.js
@@ -6,9 +6,27 @@
 </head>
 <body>
   <h1>Найдовша назва</h1>
-
+  
   <script>
-    
-  </script>
+   for (let i = 1; i <= 3; i++) {
+        let sport = prompt("Введіть вид спорту №" + i + ":");
+        if (sport === null) { 
+            alert("Скасовано");
+            break;
+        }
+        sports.push(sport.trim());
+    }
+sports.sort((a, b) => a.length - b.length);
+
+    document.write("<p style='font-size:30px; font-weight:bold; font-style:italic; color:green;'>");
+    document.write("Відсортовані назви спорту за довжиною:<br>");
+
+    for (let sport of sports) {
+        document.write(sport + "<br>");
+    }
+
+    document.write("</p>");
+</script>
+
 </body>
 </html>
